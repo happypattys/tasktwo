08d412120fd49fdcc554170cc83475c4e1890244
happypattys <etzzsaguid@gmail.com>
2025-07-27 10:51:27 +0800

    primitives

--- primitives.java
+++ primitives.java
@@ -1,1 +1,37 @@
+public class primitives {
+    public static void main(String[] args) {
+        
+        byte b = 72;         
+        short s = 51;        
+        int i = 49;          
+        float f = 2.0f;      
+        char c1 = '0';       
+        char c2 = 'r';       
+        char c3 = 'l';      
+        char c4 = 'd';       
+        boolean bool = true; 
 
+        
+        byte val_H = 72;        
+        char val_3 = '3';       
+        char val_I1 = 'I';      
+        char val_I2 = 'I';      
+        char val_0 = '0';       
+
+        char val_w = 'w';       
+        char val_zero = '0';    
+        char val_r = 'r';       
+        char val_l = 'l';       
+        char val_d = 'd';       
+
+        float val_float = 2.0f; 
+        boolean val_bool = true; 
+
+        String output = "" + (char)val_H + val_3 + val_I1 + val_I2 + val_0 + " " +
+                        val_w + val_zero + val_r + val_l + val_d + " " +
+                        val_float + " " + val_bool;
+
+        
+        System.out.println(output);
+    }
+}
